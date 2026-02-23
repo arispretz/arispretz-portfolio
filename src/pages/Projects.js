3bc4412912e43341c4a92c261b4f5374661cecb9
arispretz <117314460+arispretz@users.noreply.github.com>
2026-02-22 21:23:57 -0300

    Refactor Projects component layout and styles

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,16 +6,26 @@ import ProjectsGrid from '../components/ProjectsGrid';
 const Projects = () => {
   return (
     <>
-      <Box style={{ display: 'flex' }}>
-          <Box style={{ flex: 2, padding: '50px', backgroundColor: '#FFE0C2', width: '1500px' }}>
-          <Text as="div" style={{ textAlign: 'center', justifyContent:'center', marginLeft:'336px', marginTop: '-210px', backgroundColor:'#FFFFE9', border:'2px solid #664282', color:'purple', width:'500px', height:'100px', borderRadius:'7%'}}>
-            <h2>These are some of my Projects</h2>
-            </Text>
-          </Box>
-        </Box>
-        <ProjectsGrid />
+      <Box style={{ padding: '50px', backgroundColor: '#FFE0C2', width: '100vw' }}>
+        <Text
+          as="div"
+          style={{
+            textAlign: 'center',
+            margin: '0 auto',
+            backgroundColor: '#FFFFE9',
+            border: '2px solid #664282',
+            color: 'purple',
+            width: '500px',
+            height: '100px',
+            borderRadius: '7%'
+          }}
+        >
+          <h2>These are some of my Projects</h2>
+        </Text>
+      </Box>
+      <ProjectsGrid />
     </>
   );
-}
+};
 
 export default Projects;
