a65b9b7f94ed3aca61c40586b2ba06fd6b80dfae
arispretz <117314460+arispretz@users.noreply.github.com>
2026-02-22 22:27:05 -0300

    Remove width style from Projects component Box

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => {
   return (
     <>
       <Box style={{ display: 'flex' }}>
-          <Box style={{ flex: 2, padding: '50px', backgroundColor: '#FFE0C2', width: '100%' }}>
+          <Box style={{ flex: 2, padding: '50px', backgroundColor: '#FFE0C2' }}>
           <Text as="div" style={{ textAlign: 'center', justifyContent:'center', marginLeft:'336px', marginTop: '-210px', backgroundColor:'#FFFFE9', border:'2px solid #664282', color:'purple', width:'500px', height:'100px', borderRadius:'7%'}}>
             <h2>These are some of my Projects</h2>
             </Text>
