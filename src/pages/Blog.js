4b98d8ec847b721fdb02d9e90dd259a5b2d66d67
arispretz <117314460+arispretz@users.noreply.github.com>
2026-02-24 08:52:19 -0300

    Update Blog.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -7,7 +7,7 @@ const Blog = () => {
     <>
       <Box style={{ display: 'flex' }}>
           <Box style={{ flex: 2, padding: '50px', backgroundColor: '#DAF7A6', height: '400px' }}>
-          <Text as="div" style={{ textAlign: 'center', justifyContent:'center', marginLeft:'336px', marginTop: '-210px', backgroundColor:'#FFFFE9', border:'2px solid #664282', color:'purple', width:'500px', height:'100px', borderRadius:'7%'}}>
+          <Text as="div" style={{ textAlign: 'center', justifyContent:'center', marginLeft:'400px', marginTop: '-210px', backgroundColor:'#FFFFE9', border:'2px solid #664282', color:'purple', width:'500px', height:'100px', borderRadius:'7%'}}>
             <h2>Blog</h2>
             </Text>
           <Text as="p" style={{ textAlign: 'center', marginTop: '150px', fontWeight: 'bold', color: 'purple', fontSize: '20px'}}>
