604fe36a2f54cd3e497d558bc7f8686e67518d4b
arispretz <117314460+arispretz@users.noreply.github.com>
2026-02-24 08:52:38 -0300

    Adjust text alignment and margin in Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
     <>
       <Box style={{ display: 'flex' }}>
           <Box style={{ flex: 2, padding: '50px', backgroundColor: '#F6EEB4' }}>
-          <Text as="div" style={{ textAlign: 'center', justifyContent:'center', marginLeft:'336px', marginTop: '-210px', backgroundColor:'#FFFFE9', border:'2px solid #664282', color:'purple', width:'500px', height:'100px', borderRadius:'7%'}}>
+          <Text as="div" style={{ textAlign: 'center', justifyContent:'center', marginLeft:'400px', marginTop: '-210px', backgroundColor:'#FFFFE9', border:'2px solid #664282', color:'purple', width:'500px', height:'100px', borderRadius:'7%'}}>
             <h2>Let'work together...</h2>
             </Text>
             <Text as="div" style={{ textAlign: 'center', marginTop: '120px', paddingBottom: '-100px', color: 'purple' }}>
