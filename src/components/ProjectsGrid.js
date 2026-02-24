c49cfc9e81b05d8b275bcb4d77650a53c1defde5
arispretz <117314460+arispretz@users.noreply.github.com>
2026-02-23 21:05:46 -0300

    Refactor ProjectsGrid to remove unnecessary padding
    Removed padding from the ProjectsGrid component.

--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -19,7 +19,7 @@ const ProjectsGrid = () => {
 
   return (
     <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Box style={{ flex: 2, padding: '5px 80px 40px 80px', marginTop: '-50px', backgroundColor: '#FFE0C2' }}>
+      <Box style={{ flex: 2, marginTop: '-50px', backgroundColor: '#FFE0C2' }}>
         <Container style={{ color: 'purple' }}>
           <Grid columns="3" gap="3" rows="auto" width="auto">
             {currentProjects.map((project) => (
