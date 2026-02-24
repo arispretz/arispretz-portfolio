9dd50cc0c12780691394ac41e68f4213a87c08b8
arispretz <117314460+arispretz@users.noreply.github.com>
2026-02-23 22:12:50 -0300

    Adjust marginLeft in Home.js Text component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <>
       <Box style={{ display: 'flex' }}>
         <Box style={{ flex: 2, padding: '50px', backgroundColor: '#FFE0C2' }}>
-          <Text as="div" style={{ textAlign: 'center', justifyContent: 'center', marginLeft: '400px', marginTop: '-250px', backgroundColor: 'white', border: '2px solid #664282', color: '#450c49', width: '600px', height: '150px', borderRadius: '7%', display: 'flex', alignItems: 'center' }}>
+          <Text as="div" style={{ textAlign: 'center', justifyContent: 'center', marginLeft: '420px', marginTop: '-250px', backgroundColor: 'white', border: '2px solid #664282', color: '#450c49', width: '600px', height: '150px', borderRadius: '7%', display: 'flex', alignItems: 'center' }}>
             <Box style={{ display: 'flex', alignItems: 'center' }}>
               <Picture style={{ width: '50px', height: '50px', marginRight: '50px' }} />
               <h1>Hello, I am <Em>Ariana Spretz</Em></h1>
